ddb8c9ea0ea43e4c67524e83bb64bfe4931cd3bf
Makong <maxim.kondrachuk@zeeks.co>
2026-03-13 12:59:41 +0200

    path js changes

--- functions/go/[[path]].js
+++ functions/go/[[path]].js
@@ -1,13 +1,8 @@
-export function onRequestGet(context) {
-  const slug = new URL(context.request.url).pathname.replace(/^\/go\//, "");
-
-  const redirects = {
-    "brand-a": "https://affiliate.example.com/brand-a"
-  };
-
-  if (!redirects[slug]) {
-    return new Response("Not found", { status: 404 });
-  }
-
-  return Response.redirect(redirects[slug], 302);
+export function onRequest(context) {
+  return new Response("go function works", {
+    status: 200,
+    headers: {
+      "content-type": "text/plain; charset=UTF-8"
+    }
+  });
 }
