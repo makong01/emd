95d15b09b8014a463165c8052520ced78a4acb4e
Makong <maxim.kondrachuk@zeeks.co>
2026-03-13 14:30:07 +0200

    path js fix

--- functions/go/[[path]].js
+++ functions/go/[[path]].js
@@ -3,27 +3,5 @@ import redirectsText from "../../data/redirects.json";
 const redirects = JSON.parse(redirectsText);
 
 export function onRequest(context) {
-  const method = context.request.method;
-
-  if (method !== "GET" && method !== "HEAD") {
-    return new Response("Method Not Allowed", {
-      status: 405,
-      headers: { Allow: "GET, HEAD" }
-    });
-  }
-
-  const url = new URL(context.request.url);
-  const slug = url.pathname.replace(/^\/go\//, "").replace(/\/+$/, "");
-
-  if (!slug) {
-    return new Response("Missing redirect key", { status: 400 });
-  }
-
-  const target = redirects[slug];
-
-  if (!target) {
-    return new Response("Redirect target not found", { status: 404 });
-  }
-
-  return Response.redirect(target, 302);
+  
 }
